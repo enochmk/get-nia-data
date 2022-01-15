4e88287501e424a388c16e2875ec5f9fc040a219
Enoch Klufio <enochmk94@gmail.com>
2022-01-15 20:55:26 +0000

    refactor: move function into retryRequestsSequentially

--- index.js
+++ index.js
@@ -18,24 +18,7 @@ const main = async () => {
 		const dataset = await getRetryRequests();
 
 		logger.verbose(`Starting - Total Rows in dataset: ${dataset.length}.`);
-		for (i = 0; i < dataset.length; i++) {
-			const row = dataset[i];
-			logger.verbose(`Processing data ${i + 1}/${dataset.length}`);
-
-			const { ID, SURNAME, PIN_NUMBER } = row;
-
-			const cardValid = await isVerified(PIN_NUMBER, SURNAME);
-			if (cardValid) {
-				await updateRequest(ID, 'TRUE', 'Data already in system');
-				continue;
-			}
-
-			// MAKE API CALL;
-			const response = await verificationAPI(PIN_NUMBER, SURNAME);
-			const status = response.status ? 'TRUE' : 'FALSE';
-			const message = response.message;
-			await updateRequest(ID, status, message);
-		}
+		await retryRequestsSequentially(dataset);
 		logger.verbose(`Ending - Total Rows in dataset: ${dataset.length}.`);
 	} catch (error) {
 		logger.error(error);
@@ -43,6 +26,26 @@ const main = async () => {
 		logger.verbose('Database connection closed');
 		await conn.close();
 		process.exit(1);
+	}
+};
+
+const retryRequestsSequentially = async (dataset) => {
+	for (i = 0; i < dataset.length; i++) {
+		const row = dataset[i];
+		const { ID, SURNAME, PIN_NUMBER } = row;
+
+		logger.verbose(`Processing data ${i + 1}/${dataset.length}`);
+		const cardValid = await isVerified(PIN_NUMBER, SURNAME);
+		if (cardValid) {
+			await updateRequest(ID, 'TRUE', 'Data already in system');
+			continue;
+		}
+
+		const response = await verificationAPI(PIN_NUMBER, SURNAME);
+		const status = response.status ? 'TRUE' : 'FALSE';
+		const message = response.message;
+
+		await updateRequest(ID, status, message);
 	}
 };
 
